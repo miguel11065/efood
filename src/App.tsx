609a6bf421971a1c6e772e4ed190d69b9bb0dbcd
Miguel Leal <miguel11065@gmail.com>
2023-11-02 14:00:34 -0300

    Construindo o header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Header from './components/Header'
 import { GlobalCss } from './styles'
 
 function App() {
   return (
     <>
       <GlobalCss />
-      <div>ola mundo</div>
+      <div>
+        <Header />
+      </div>
     </>
   )
 }
