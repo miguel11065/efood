f0329561886721acecd5bed31600b6d7a056999c
Miguel Leal <miguel11065@gmail.com>
2023-11-02 18:29:09 -0300

    Construindo o footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import { GlobalCss } from './styles'
 import Rotas from './routes'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalCss />
       <Rotas />
+      <Footer />
     </BrowserRouter>
   )
 }
