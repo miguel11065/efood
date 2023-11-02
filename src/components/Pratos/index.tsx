930913d10acb65efbdea3f0f8c77a7e4e99bd756
Miguel Leal <miguel11065@gmail.com>
2023-11-02 19:45:14 -0300

    Criando a modal

--- src/components/Pratos/index.tsx
+++ src/components/Pratos/index.tsx
@@ -1,4 +1,16 @@
-import { Botao, Card, Descricao, PratoImg, Titulo } from './styles'
+import {
+  Botao,
+  Card,
+  Descricao,
+  Fechar,
+  Modal,
+  ModalContainer,
+  ModalContent,
+  PratoImg,
+  Titulo
+} from './styles'
+import close from '../../assets/images/fechar.svg'
+import { useState } from 'react'
 
 export type Props = {
   image: string
@@ -8,14 +20,36 @@ export type Props = {
 }
 
 const PratosLista = ({ image, title, description }: Props) => {
+  const [modalEstaAberto, setModalEstaAberta] = useState(false)
+
   return (
     <>
       <Card className="container">
         <PratoImg src={image} />
         <Titulo>{title}</Titulo>
         <Descricao>{description}</Descricao>
-        <Botao>Adcionar ao carrinho</Botao>
+        <Botao onClick={() => setModalEstaAberta(true)}>
+          Adcionar ao carrinho
+        </Botao>
       </Card>
+      <Modal className={modalEstaAberto ? 'visivel' : ''}>
+        <ModalContent className="container">
+          <ModalContainer>
+            <img src={image} />
+            <div>
+              <h4>{title}</h4>
+              <p>{description}</p>
+              <span>Rende de 1 a 2 pessoas</span>
+              <button>Adcionar ao carrinho - R$ 100,00</button>
+            </div>
+          </ModalContainer>
+          <Fechar src={close} onClick={() => setModalEstaAberta(false)} />
+        </ModalContent>
+        <div
+          className="overlay"
+          onClick={() => setModalEstaAberta(false)}
+        ></div>
+      </Modal>
     </>
   )
 }
